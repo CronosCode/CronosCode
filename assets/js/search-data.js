8da9971764de08e437f270b7e8a9a5d95aa044a4
CronosCode <danielbranysh@gmail.com>
2025-08-12 23:06:24 +0000

    Deploying to gh-pages from @ CronosCode/CronosCode@5dbd406ff804304a2264654291af1ac4366d90d2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,8 +9,8 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/CronosCode/";
     },
-  },{id: "nav-bookshelf",
-          title: "bookshelf",
+  },{id: "nav-bookshelf-with-links-below",
+          title: "Bookshelf with Links below",
           description: "",
           section: "Navigation",
           handler: () => {
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "A small collection of my projects, may be a WIP.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/CronosCode/projects/";
@@ -46,7 +46,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          description: "Supplemental Instructor and Maths / CS Tutor",
           section: "Navigation",
           handler: () => {
             window.location.href = "/CronosCode/teaching/";
@@ -65,38 +65,27 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/CronosCode/blog/";
               },
-            },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
+            },{id: "post-multi-head-self-attention-and-why-you-need-the-math-by-daniel-branysh-jul-2025-medium",
         
-          title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
+          title: 'Multi-Head Self-Attention and Why You Need The Math | by Daniel Branysh |... <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
         
-        description: "We’re sharing updates across our Gemini family of models and a glimpse of Project Astra, our vision for the future of AI assistants.",
+        description: "The transformer architecture has dramatically changed the landscape of natural language processing, enabling powerful language models such as GPT and BERT to excel at diverse text-based tasks. At the…",
         section: "Posts",
         handler: () => {
           
-            window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
-          
-        },
-      },{id: "post-displaying-external-posts-on-your-al-folio-blog",
-        
-          title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
+            window.open("https://medium.com/@danielbranysh/multi-head-self-attention-and-why-you-need-the-math-a622b036e80f", "_blank");
           
         },
       },{id: "books-a-first-course-in-probability",
           title: 'A First Course in Probability',
-          description: "",
+          description: "A First Course in Probability",
           section: "Books",handler: () => {
-              window.location.href = "/CronosCode/books/a-first-course-in-probability/";
+              window.location.href = "/CronosCode/books/Daniel's-List";
             },},{id: "books-probability-and-computing",
           title: 'Probability and Computing',
-          description: "",
+          description: "Probability and Computing by Michael Mitzenmacher and Eli Upfal",
           section: "Books",handler: () => {
-              window.location.href = "/CronosCode/books/probability-and-computing/";
+              window.location.href = "/CronosCode/books/Daniel's-List";
             },},{
         id: 'social-email',
         title: 'email',
